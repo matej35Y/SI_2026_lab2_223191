a926ae18ad316e4fa383b16ddc1cc2a5ec6be3bc
Matej Machevski <matejmacevski35@gmail.com>
2026-05-10 16:59:59 +0200

    Added test cases for very statement, every branch and multiple condition

--- SI_lab2_223191/src/test/java/SI2026Lab2Test.java
+++ SI_lab2_223191/src/test/java/SI2026Lab2Test.java
@@ -15,4 +15,82 @@ public class SI2026Lab2Test {
         library.addBook(new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy"));
     }
 
+    @Test
+    public void searchBookEveryStatementTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.searchBookByTitle("");
+        });
+
+        List<Book> found = library.searchBookByTitle("Clean Code");
+        assertNotNull(found);
+
+        List<Book> notFound = library.searchBookByTitle("Harry Potter");
+        assertNull(notFound);
+
+        library.borrowBook("The Hobbit", "J.R.R. Tolkien");
+        List<Book> borrowed = library.searchBookByTitle("The Hobbit");
+        assertNull(borrowed);
+    }
+
+    @Test
+    public void borrowBookEveryBranchTest() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.borrowBook("", "J.R.R. Tolkien");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.borrowBook("The Hobbit", "");
+        });
+
+        assertDoesNotThrow(() -> {
+            library.borrowBook("The Hobbit", "J.R.R. Tolkien");
+        });
+
+        assertThrows(RuntimeException.class, () -> {
+            library.borrowBook("The Hobbit", "J.R.R. Tolkien");
+        });
+
+        assertThrows(RuntimeException.class, () -> {
+            library.borrowBook("Harry Potter", "J.K. Rowling");
+        });
+
+
+
+    }
+
+    @Test
+    public void searchBookMultipleConditionTest() {
+
+        List<Book> result1 = library.searchBookByTitle("Clean Code");
+        assertNotNull(result1);
+
+        library.borrowBook("The Hobbit", "J.R.R. Tolkien");
+        List<Book> result2 = library.searchBookByTitle("The Hobbit");
+        assertNull(result2);
+
+        List<Book> result3 = library.searchBookByTitle("Harry Potter");
+        assertNull(result3);
+    }
+
+    @Test
+    public void borrowBookMultipleConditionTest() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.borrowBook("", "");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.borrowBook("", "J.R.R. Tolkien");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.borrowBook("The Hobbit", "");
+        });
+
+        assertDoesNotThrow(() -> {
+            library.borrowBook("The Hobbit", "J.R.R. Tolkien");
+        });
+    }
+
 }
